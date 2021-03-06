40e9cb9d1120dc08d065fc3da57687565cf8861d
Ang Jun Ray <angjunray@live.com>
2021-03-06 23:32:40 +0800

    tried classes stuff but didn't work

---
+++ src/shooter.js
@@ -0,0 +1,16 @@
+class Player{
+    constructor(X){
+        this.X = X;
+        this.Y = Y;
+        this.drawInstance();
+    }
+    shiftPosition(offset) {
+        new Player(this.X + offset);
+    }
+    drawInstance() {
+        triangle(this.X - 10, this.Y, this.X + 10, this.Y, this.X, this.Y + 10)
+    }
+}
+
+
+currentPlayer = new Player(width/2);
---
+++ src/sketch.js
@@ -0,0 +1,12 @@
+let Y, currentWidth, currentHeight;
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+    Y = windowHeight * 5 / 6;
+}
+
+function draw() {
+    currentHeight = height;
+    currentWidth = width;
+}
+
