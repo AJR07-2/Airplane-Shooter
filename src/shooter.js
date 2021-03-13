6054744cd7d8a774d1479d1d7b3da1f3c6cc32dc
Ang Jun Ray <angjunray@live.com>
2021-03-13 23:29:23 +0800

    Bullet firing works

--- src/shooter.js
+++ src/shooter.js
@@ -39,7 +39,7 @@ class Player{
     }
     shoot() {
         this.BulletUID = this.uid;
-        this.Bullet[this.BulletUID] = (new Bullet(this.X, this.Y, this.BulletUID));
+        this.Bullet[this.BulletUID] = (new Bullet(this.pos.x, this.pos.y, this.BulletUID));
         this.BulletExists = true;
     }
     uid() {
@@ -49,26 +49,24 @@ class Player{
  
 class Bullet{
     constructor(X, Y, uid) {
-        this.X = X + 0.01;
-        this.Y = Y + 0.01;
+        this.pos = createVector(X + 0.01, Y + 0.01);
         this.rotateDeg = currentPlayer.rotateDeg;
         this.update();
         this.uid = uid;
     }
     update() {
-        this.previousX = this.X;
-        this.previousY = this.Y;
-        this.Ymovement = Math.cos(this.rotateDeg * Math.PI / 180) * -10;
-        this.Xmovement = Math.sin(this.rotateDeg * Math.PI / 180) * 10;
+        this.prevPos = createVector(this.pos.x, this.pos.y);
+        let yForce = (Math.cos(this.rotateDeg * Math.PI / 180) * -10) * 2;
+        let xForce = (Math.sin(this.rotateDeg * Math.PI / 180) * 10) * 2;
+        this.force = createVector(xForce, yForce);
         this.checkDeletion();
-        this.Y += this.Ymovement;
-        this.X += this.Xmovement;
+        this.pos.add(this.force);
         this.drawInstance();
     }
     checkDeletion() {
-        if (this.Y <= 0 || this.Y >= height || this.X <= 0 || this.X >= width) {
+        if (this.pos.y <= 0 || this.pos.y >= height || this.pos.x <= 0 || this.pos.x >= width) {
             push();
-            translate(this.previousX, this.previousY);
+            translate(this.prevPos.x, this.prevPos.y);
             rotate(this.rotateDeg);
             erase();
             ellipse(0, 0, width / 40, height / 20);
@@ -82,7 +80,7 @@ class Bullet{
         fill("red");
         //erase previous element
         push();
-        translate(this.previousX, this.previousY);
+        translate(this.prevPos.x, this.prevPos.y);
         rotate(this.rotateDeg);
         erase();
         ellipse(0, 0, width / 40, height / 20);
@@ -90,7 +88,7 @@ class Bullet{
         pop();
         //create new element
         push();
-        translate(this.X, this.Y);
+        translate(this.pos.x, this.pos.y);
         rotate(this.rotateDeg);
         ellipse(0, 0, width / 40, height / 20);
         pop();
